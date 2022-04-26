3efd46b49f39a650308d1ab28e4f26cc78b9ec96
Sebastian Gatica <expandiendoartemusical@gmail.com>
2022-04-26 17:07:06 -0300

    gestor-usuarios-react

---
+++ gestor-usuarios/src/App.js
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+import Card from './components/Card';
+import Container from './components/Container';
+import UserForm from './components/UserForm';
+
+const App = () =>{
+  const [usuarios, setUsuarios] = useState([])
+ 
+
+ const submit = usuario => {
+  setUsuarios([
+    ...usuarios,
+    usuario,
+  ])
+
+ }
+
+  
+  console.log(usuarios)
+
+  return (
+
+<div style={{ marginTop:'9%' }}>   
+<Container>
+  <Card>
+    <div style={{padding:20}}>
+       <UserForm submit={submit} />
+    </div>
+  </Card>
+  <Card>
+    <ul>
+      {usuarios.map(x => 
+        <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>)}
+    </ul>
+  </Card>
+    
+</Container>
+</div> 
+
+  )
+}
+
+export default App;
